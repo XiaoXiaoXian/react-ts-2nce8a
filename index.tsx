8442ee9d30f3857a970063c76ebbd62c15b574fb
XiaoXiaoXian <476639696@qq.com>
2021-12-21 17:38:43 +0800

    添加 antd 库

--- index.tsx
+++ index.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
+import { Button } from 'antd';
 
 interface AppProps {}
 interface AppState {
@@ -21,6 +22,7 @@ class App extends Component<AppProps, AppState> {
       <div>
         <Hello name={this.state.name} />
         <p>Start editing to see some magic happen :)</p>
+        <Button />
       </div>
     );
   }
